ef514f8c54c33531dfece218d3f269c6ce41fe81
Stuart Harlan <stuart.harlan@outlook.com>
2022-10-31 11:35:56 -0500

    ADDR8LB and ADDR8HB need to be on zero page to work

--- asm/mem.s
+++ asm/mem.s
@@ -33,6 +33,11 @@ VIAIFRCA1		= %00000010
 VIAIFRCB1		= %00010000
 PRSFLGERR		= %00000001
 
+ADDR8LZ			= $0080	; addr low zero page
+ADDR8HZ			= $0081 ; addr high zero page
+ADDR8LB			= $0082 ; addr low for print buffer (SEROUT)
+ADDR8HB			= $0083 ; addr high for print buffer (SEROUT)
+
 CTP				= $1000	; char to print
 CBUFN			= $1001 ; num chars in cmdbuf
 CBUFF			= $1002	; cmd buffer flags
@@ -43,9 +48,15 @@ SOVAL			= $1004 ; the byte value to output to serial
 STP				= $1005 ; the LO byte addr location for serial out
 PRSFLG			= $1006 ; cmd parse flags
 						; bit 0: 1 error; 0 no error
-ADDR8L			= $0080	; addr low
-ADDR8H			= $0081 ; addr high
+OUTBUFP			= $1007 ; output buffer position
+OUTBUFC			= $1008 ; temp char for output buffer processing
+B2CIN			= $1009 ; byte to char input
+B2COUTH			= $100a ; byte to char high nibble
+B2COUTL			= $100b ; byte to char lo nibble
+ADDR8LM			= $100c ; addr low non zero page
+ADDR8HM			= $100d ; addr high non zero page
 CMDBUF			= $1100	; command buffer, 256 bytes $1100 to $11ff
+OUTBUF			= $1200 ; output buffer, 256 bytes $1200 to $12ff
 
 CBFUNXFLG		= %00000001		; unexected data flag
 CBFUNXFLGB		= %11111110		; inverse of unexected data flag
@@ -54,6 +65,9 @@ CBFXEQFLGB		= %11111101		; inverse of XEQ flag
 LF				= $0a
 LONIBBLE		= %00001111
 DOLLAR			= '$'
+COLON			= ':'
+SPACE			= ' '
+XCAP			= 'X'
 
 	; Memory map:
 	; $0000 - $7fff = RAM
@@ -101,10 +115,12 @@ RESET:
 	lda #$55
 	sta $0900
 
-	stz CTP			; memory initialization
-	stz CBUFN
-	stz CBUFF
-	stz SOVAL
+	stz CTP			; Characer To Print
+	stz CBUFN		; num of chars in cmd buffer
+	stz CBUFF		; command buffer flags
+	stz SOVAL		; Serial Output VALue
+	stz OUTBUF		; OUTput BUFfer
+	stz OUTBUFP		; OUTput BUFfer Position
 
 	lda #%10101010	; setup pcr for serial out on a and b
 					; pulse output (101) bits 3,2,1
@@ -125,9 +141,9 @@ RESET:
 	cli				; tell processor to respond to interrupts
 
 	lda TXTRDYA		; output ready msg on serial
-	sta ADDR8L		; load the address of the text to ram in 
+	sta ADDR8LB		; load the address of the text to ram in 
 	lda TXTRDYA+1	; the zero page
-	sta ADDR8H		; and call SEROUT
+	sta ADDR8HB		; and call SEROUT
 	jsr SEROUT		; to write it to serial
 
 WAIT3:
@@ -148,9 +164,9 @@ WAIT3:
 	; output syntax error
 
 	lda TXTSNTEA	; output syntax error msg
-	sta ADDR8L
+	sta ADDR8LB
 	lda TXTSNTEA+1
-	sta ADDR8H
+	sta ADDR8HB
 	jsr SEROUT	
 
 CONT2:
@@ -191,7 +207,7 @@ CMDDUMP1:
 	asl
 	asl
 	asl				; left shift a four times to make it hi nibble
-	sta ADDR8H		; store in addr high
+	sta ADDR8HZ		; store in addr high
 	inx				; next buffer char
 	lda CMDBUF,X	; get char
 	sta PCS1		; store the current char
@@ -210,8 +226,8 @@ ACASE2:
 
 	tya
 	clc				; clear carry flag
-	adc ADDR8H		; add low nibble to ADDRHI
-	sta ADDR8H		; store back in ADDRHI
+	adc ADDR8HZ		; add low nibble to ADDRHI
+	sta ADDR8HZ		; store back in ADDRHI
 	inx				; next buffer char
 	lda CMDBUF,X	; get char
 	sta PCS1		; store the current char
@@ -227,7 +243,7 @@ ACASE3:
 	asl
 	asl
 	asl
-	sta ADDR8L
+	sta ADDR8LZ
 	inx
 	lda CMDBUF,X	; get char
 	sta PCS1		; store the current char
@@ -240,24 +256,25 @@ ACASE4:
 
 	tya
 	clc				; clear carry flag
-	adc ADDR8L
-	sta ADDR8L
-					
-	; for debugging
-	; output low byte and hi byte
-	lda ADDR8L
-	sta SOVAL
-	jsr SOBYTE	
+	adc ADDR8LZ
+	sta ADDR8LZ
+	sta ADDR8LM		; also store in LM
 
-	lda ADDR8H
-	sta SOVAL
-	jsr SOBYTE	
+	lda ADDR8HZ
+	sta ADDR8HM		; also store in HM		
+
+	; get the value at that address and
+	; store in SOVAL so we can output
 
 	ldy #$00		; address is in ADDRHI ADDRLO
-	lda ($80),Y		; load value from address at 0080 (ADDR8H/ADDR8L)
+	lda ($80),Y		; load value from address at 0080 (ADDR8HZ/ADDR8LZ)
 					; with no offset
 	sta SOVAL
-	jsr SOBYTE
+
+	; addr8l and addr8h are already populated
+	; so just call SYBYTEADDR to output
+
+	jsr SOBYTEADDR
 	
 	jmp PARSEDONE
 
@@ -269,6 +286,127 @@ PARSEDONE:
 	rts
 
 ; ****************************************
+; * reset output buffer
+; ****************************************
+RESETOUTB:
+	stz OUTBUFP
+	stz OUTBUF
+	rts
+
+; ****************************************
+; * add byte to output buffer 
+; ****************************************
+ADDOBCHAR:
+	lda #$ff
+	cmp OUTBUFP		; output buffer should only hold
+					; 254 characters because the last
+					; should be a null byte (00)
+	beq ADDOBCHAR1	; so, if the current position is 0xff
+					; ignore this request and return
+
+	lda OUTBUFC		; load the character from memory
+	ldx OUTBUFP		; load the next position (0x00-0xfe)
+	sta OUTBUF,X	; store the char in buffer
+	inc OUTBUFP		; increment position
+	lda #$00
+	sta OUTBUF,X	; store null byte in next pos
+					; this will ensure it always ends
+					; with a null byte
+
+ADDOBCHAR1:
+	rts
+
+; ****************************************
+; * SOBYTEADDR - ads address
+; * Output in the format:
+; *   $XXXX: XX
+; ****************************************
+SOBYTEADDR:
+	lda #DOLLAR
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add dollar sign
+
+	; address high
+	lda ADDR8HM		; convert ADDR8HM to chars
+	sta B2CIN
+	jsr BYTE2CHAR
+	lda B2COUTH
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add high nibble
+	lda B2COUTL
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add lo nibble
+
+	; address low
+	lda ADDR8LM		; convert ADDR8HM to chars
+	sta B2CIN
+	jsr BYTE2CHAR
+	lda B2COUTH
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add high nibble
+	lda B2COUTL
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add lo nibble
+
+	lda #COLON
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add colon
+
+	lda #SPACE
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add space
+
+	; SOVAL byte
+	lda SOVAL		; convert SOVAL to chars
+	sta B2CIN
+	jsr BYTE2CHAR
+	lda B2COUTH
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add high nibble
+	lda B2COUTL
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add lo nibble
+
+	lda #LF
+	sta OUTBUFC
+	jsr ADDOBCHAR	; add linefeed
+
+	; print the buffer
+	lda OUTBUF		; output OUTBUF
+	sta ADDR8LB
+	lda OUTBUF+1
+	sta ADDR8HB
+	jsr SEROUT	
+
+	rts
+
+; ****************************************
+; * BYTE 2 CHAR
+; ****************************************
+BYTE2CHAR:
+	; output the high nibble
+
+	lda B2CIN
+	lsr				; shift bits right 4 times
+	lsr
+	lsr
+	lsr
+	and #LONIBBLE	; get lower nibble of a	
+	tax				; lower nibble is offset (0-15)
+	lda TXTDGTS,X	; get char
+	sta B2COUTH
+
+	; output the lo nibble
+
+	lda B2CIN
+	and #LONIBBLE	; get lower nibble of a	
+	tax				; put nibble in x (0-15)
+	lda TXTDGTS,X	; get digit from digit table offset x
+	sta B2COUTL
+
+	rts
+
+; ****************************************
 ; * serial out byte
 ; * byte is in SOVAL
 ; ****************************************
@@ -277,25 +415,31 @@ SOBYTE:
 	sta CTP
 	jsr SEROUTCHAR
 
+	lda SOVAL
+	sta B2CIN
+	jsr BYTE2CHAR
+
 	; output the high nibble
 
-	lda SOVAL
-	lsr				; shift bits right 4 times
-	lsr
-	lsr
-	lsr
-	and #LONIBBLE	; get lower nibble of a	
-	tax				; lower nibble is offset (0-15)
-	lda TXTDGTS,X	; get char
+	;lda SOVAL
+	;lsr				; shift bits right 4 times
+	;lsr
+	;lsr
+	;lsr
+	;and #LONIBBLE	; get lower nibble of a	
+	;tax				; lower nibble is offset (0-15)
+	;lda TXTDGTS,X	; get char
+	lda B2COUTH
 	sta CTP
 	jsr SEROUTCHAR	; output next char
 
 	; output the lo nibble
 
-	lda SOVAL
-	and #LONIBBLE	; get lower nibble of a	
-	tax				; put nibble in x (0-15)
-	lda TXTDGTS,X	; get digit from digit table offset x
+	;lda SOVAL
+	;and #LONIBBLE	; get lower nibble of a	
+	;tax				; put nibble in x (0-15)
+	;lda TXTDGTS,X	; get digit from digit table offset x
+	lda B2COUTL
 	sta CTP
 	jsr SEROUTCHAR	; output the char
 
@@ -328,12 +472,12 @@ SOCMDBUF2:
 
 ; ****************************************
 ; * serial out
-; * location of text is ADDR8L/ADDR8H
+; * location of text is ADDR8LB/ADDR8HB
 ; ****************************************
 SEROUT:
 	ldy #$00		; start at char 0
 SEROUT1:
-	lda (ADDR8L),Y	; load next char into a
+	lda (ADDR8LB),Y	; load next char into a
 	beq SEROUT2		; if a = 0 (end of string) -> done
 	sta CTP			; store into CTP memory
 	jsr SEROUTCHAR	; send to serial out
